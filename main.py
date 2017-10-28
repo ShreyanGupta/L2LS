2aef9ee8ac725c1e5471b74e0b66b41564901d5c
Shreyan <shreyan.gupta96@gmail.com>
2017-10-29 00:21:23 +0530

    added cuda

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import numpy as np
 import torch
 import torch.nn as nn
-
-from dataset import KittyDataset
-import scipy.misc as m
+import numpy as np
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
+
+from dataset import KittyDataset
 from stereocnn import StereoCNN
 
 # TODO(AA) : CUDA compatible? / Can we implement correlation in C++/GPU (faster)?
@@ -14,14 +13,15 @@ from stereocnn import StereoCNN
 # TOGO(??) : Complete the CRF wala part
 
 # Global variables
-k = 128
-learning_rate = 1e-2
+k = 256
+learning_rate = 1e-3
 momentum = 0.1
 
 batch_size = 1
 num_workers = 4
 # DATA_DIR = '/Users/ankitanand/Desktop/Stereo_CRF_CNN/Datasets/Kitty/data_scene_flow'
-DATA_DIR = '/Users/Shreyan/Downloads/Datasets/Kitty/data_scene_flow'
+# DATA_DIR = '/Users/Shreyan/Downloads/Datasets/Kitty/data_scene_flow'
+DATA_DIR = '/home/ankit/Stereo_CNN_CRF/L2LS/Datasets/Kitty/data_scene_flow'
 
 def main():
   train_set = KittyDataset(DATA_DIR)
@@ -31,12 +31,20 @@ def main():
   loss_fn = nn.CrossEntropyLoss()
   optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
+  if torch.cuda.is_available():
+    model = model.cuda()
+
   for epoch in range(100):
     print("epoch", epoch)
     for i, data in enumerate(train_loader):
       left_img, right_img, labels = data
       # No clamping might be dangerous
       # labels.clamp_(0,k-1)
+
+      if torch.cuda.is_available():
+        left_img = left_img.cuda()
+        right_img = right_img.cuda()
+        labels = labels.cuda()
       
       left_img = Variable(left_img)
       right_img = Variable(right_img)
@@ -51,6 +59,7 @@ def main():
       loss.backward()
       optimizer.step()
       print("loss", i, loss.data[0])
+      break
 
 if __name__ == "__main__":
   main()
