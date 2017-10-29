b68335fba743b67cada94d743ff9e19866e85eef
Ankit Anand <ankitanand@Ankits-MacBook-Pro.local>
2017-10-29 18:45:26 +0530

    middlebury_added

--- dataset.py
+++ dataset.py
@@ -3,17 +3,18 @@ import collections
 import os
 import scipy.misc as m
 import numpy as np
+from load_pfm import load_pfm
 
 class KittyDataset(Dataset):
   """Kitty dataset"""
   def __init__(self, root, split='training', transform=None):
-    self.root = root
+    self.root = root+"/Kitty/data_scene_flow"
     self.split = split
     self._transform = transform
     self.files = collections.defaultdict(list)
     for split in ['training', 'testing']:
-      file_list = os.listdir(root + '/' + split+'/myimage_2')
-      self.files[split] = file_list
+        self.files[split] = os.listdir(root + '/' + split+'/myimage_2')
+
 
   def __len__(self):
     return len(self.files[self.split])
@@ -30,12 +31,10 @@ class KittyDataset(Dataset):
 
     left_img = m.imread(left_img_path)
     left_img = np.array(left_img, dtype=np.float32)
-    left_img = np.pad(left_img, ((0,376-left_img.shape[0]), (0,1242-left_img.shape[1]), (0,0)), 'constant', constant_values=0)
     left_img = left_img.transpose(2,0,1)
 
     right_img = m.imread(right_img_path)
     right_img = np.array(right_img,dtype=np.float32)
-    right_img = np.pad(right_img, ((0,376-right_img.shape[0]), (0,1242-right_img.shape[1]), (0,0)), 'constant', constant_values=0)
     right_img = right_img.transpose(2,0,1)
     
     # Normalizing images
@@ -54,15 +53,50 @@ class KittyDataset(Dataset):
 
 class MiddleburyDataset(Dataset):
   """Middlebury dataset"""
-  def __init__(self, path):
-    self.path = path
+
+  def __init__(self, root, split='training', Resolution='Q',transform=None):
+      self.root = root + "Middlebury/Eval3/"
+      self.split = split
+      self._transform = transform
+      self.files = collections.defaultdict(list)
+      self.Resolution=Resolution
+      for split in ['training', 'test']:
+          print(self.root + split + Resolution)
+          os.system("rm "+self.root + split + Resolution+"/.DS_Store ")
+          self.files[split] = os.listdir(self.root + split + Resolution )
+
+
 
   def __len__(self):
-    return -1
+      return len(self.files[self.split])
 
   def __getitem__(self, i):
-    """
-      Get the ith item from the dataset
-      Return : left_img, right_img, target
-    """
-    pass
+      """
+        Get the ith item from the dataset
+        Return : left_img, right_img, target
+      """
+
+      img_name = self.files[self.split][i]
+      left_img_path = self.root  + self.split + self.Resolution + '/' + img_name+ '/' + 'im0.png'
+      right_img_path = self.root + self.split + self.Resolution + '/' + img_name+ '/' + 'im1_rectified.png'
+
+      lbl_path = self.root + "GT-" + self.split + self.Resolution + '/' + img_name+ '/' +'disp0GT.pfm'
+      left_img = m.imread(left_img_path)
+      left_img = np.array(left_img, dtype=np.float32)
+      left_img = left_img.transpose(2, 0, 1)
+
+      right_img = m.imread(right_img_path)
+      right_img = np.array(right_img, dtype=np.float32)
+      right_img = right_img.transpose(2, 0, 1)
+
+      # Normalizing images
+      left_img = (left_img - left_img.mean()) / left_img.std()
+      right_img = (right_img - right_img.mean()) / right_img.std()
+
+      lbl = load_pfm(lbl_path)
+      lbl=lbl[0]
+      lbl[lbl==np.inf]=-1
+      if self._transform:
+          left_img, right_img, lbl = self.transform(img, lbl)
+
+      return left_img, right_img, lbl
